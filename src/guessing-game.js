4ec6ea3c0131fb93a5c380a307ab1efbe8caa3d9
VikinGSpb <imbot@inbox.ru>
2019-10-03 22:05:51 +0300

    feat: first version

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,33 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.state = 'lower';
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        let oldMin = this.min;
+        let oldMax = this.max;
+        if(this.state == 'lower')
+        {
+            this.max = Math.floor((this.max + this.min) / 2);
+        } else if(this.state == 'greater'){
+            this.min = Math.ceil((this.max + this.min) / 2);
+        }
+        return Math.floor((oldMin+oldMax) / 2);
     }
 
     lower() {
-
+        this.state = 'lower';
     }
 
     greater() {
-
+        this.state = 'greater';
     }
 }
 
